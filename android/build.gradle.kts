2cf4328645bfa1684eef7e38ed06913d36efe8bf
b3bas <benas@gmail.com>
2026-07-02 02:00:53 +0700

    update

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,4 +2,6 @@ plugins {
     id("com.android.application") version "8.7.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.2" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.23.8" apply false
 }
